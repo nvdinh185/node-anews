0b54ea78f71695689bd6f29837967b7cc9e35e19
nvdinh185 <nvdinh0766777123@gmail.com>
2023-03-08 14:14:45 +0700

    add middle

--- news/news.controller.js
+++ news/news.controller.js
@@ -1,10 +1,8 @@
 ﻿const express = require('express');
 const router = express.Router();
-const formidable = require('formidable');
-const fs = require('fs');
-const systempath = require('path');
 
 const newsHandler = require('./news.handler');
+const newsMiddleware = require('./news.middleware');
 
 // routes
 router.get('/list-news', getListNews);
@@ -12,7 +10,7 @@ router.get('/list-news-by-cat', getListNewsByCat);
 router.get('/detail', getNewsDetail);
 router.get('/cats', getCategories);
 
-router.post('/save-contact', postContact);
+router.post('/save-contact', newsMiddleware.uploadFile, postContact);
 
 module.exports = router;
 
@@ -41,48 +39,7 @@ function getCategories(req, res, next) {
 }
 
 function postContact(req, res, next) {
-    const dirUpload = 'upload_files';
-    if (!fs.existsSync(dirUpload)) fs.mkdirSync(dirUpload);
-    const form = new formidable.IncomingForm();
-    form.uploadDir = dirUpload;
-    form.parse(req, async (err, fields, files) => {
-        var formData = {};
-        if (err) {
-            res.writeHead(403, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.end(JSON.stringify({ message: 'Parse Formdata Error', error: err }));
-        } else {
-            for (var key in fields) {
-                formData[key] = fields[key];
-            }
-            var isNotEntry = !(Object.entries(files).length === 0 && files.constructor === Object);
-            var key = "file";
-            if (isNotEntry) {
-                //đường dẫn thực file upload lên
-                var filenameStored = `${dirUpload}${systempath.sep}${Date.now()}_${files[key].originalFilename}`;
-
-                var oldpath = files[key].filepath;
-                var newpath = filenameStored
-                //chuyển file từ thư mục temp sang thư mục upload_files
-                try {
-                    await new Promise((resolve, reject) => {
-                        fs.rename(oldpath, newpath, err => {
-                            if (err) {
-                                reject("Error..." + err);
-                            } else {
-                                resolve('OK');
-                            }
-                        });
-                    })
-                } catch (err) { }
-
-                formData[key] = filenameStored;
-            } else {
-                formData[key] = '';
-            }
-            // console.log(formData);
-            newsHandler.postContact(formData)
-                .then(result => res.json(result))
-                .catch(err => next(err));
-        }
-    })
+    newsHandler.postContact(req.form_data)
+        .then(result => res.json(result))
+        .catch(err => next(err));
 }
---
+++ news/news.middleware.js
@@ -0,0 +1,53 @@
+const formidable = require('formidable');
+const fs = require('fs');
+const systempath = require('path');
+
+module.exports = {
+    uploadFile
+}
+
+function uploadFile(req, res, next) {
+    const dirUpload = 'upload_files';
+    if (!fs.existsSync(dirUpload)) fs.mkdirSync(dirUpload);
+    const form = new formidable.IncomingForm();
+    form.uploadDir = dirUpload;
+    form.parse(req, async (err, fields, files) => {
+        var formData = {};
+        if (err) {
+            res.writeHead(403, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(JSON.stringify({ message: 'Parse Formdata Error', error: err }));
+        } else {
+            for (var key in fields) {
+                formData[key] = fields[key];
+            }
+            var isNotEntry = !(Object.entries(files).length === 0 && files.constructor === Object);
+            var key = "file";
+            if (isNotEntry) {
+                //đường dẫn thực file upload lên
+                var filenameStored = `${dirUpload}${systempath.sep}${Date.now()}_${files[key].originalFilename}`;
+
+                var oldpath = files[key].filepath;
+                var newpath = filenameStored
+                //chuyển file từ thư mục temp sang thư mục upload_files
+                try {
+                    await new Promise((resolve, reject) => {
+                        fs.rename(oldpath, newpath, err => {
+                            if (err) {
+                                reject("Error..." + err);
+                            } else {
+                                resolve('OK');
+                            }
+                        });
+                    })
+                } catch (err) { }
+
+                formData[key] = filenameStored;
+            } else {
+                formData[key] = '';
+            }
+            // console.log(formData);
+            req.form_data = formData;
+            next();
+        }
+    })
+}
